692a087fbcf647d64a4cba749fcf95f537115220
Andreas Lønning Reiten <andreas.reiten@ntnu.no>
2014-08-02 23:16:17 +0200

    Facilitate for save/load

--- qxopengl/glsl/rect_hl_2d_tex.f.glsl
+++ qxopengl/glsl/rect_hl_2d_tex.f.glsl
@@ -15,38 +15,13 @@ void main(void)
     // If outside of bounds
     if ((f_texpos.x < f_bounds.x) || (f_texpos.x > f_bounds.z) || (f_texpos.y < f_bounds.y) || (f_texpos.y > f_bounds.w))
     {
-//        color.xyz = 1.0 - color.xyz;
         float average = 0.21 * color.x + 0.72 * color.y + 0.07 * color.z;
-//        color.x = 0.0;
-//        color.y = 0.0;
-//        color.z = 0.0;
-//        color.w = 1.0;
-//        color.xyz = vec3(average);
-//        color.w = 1.0;
-//        vec4 dark = (vec4)(1.0,0.0,0.0,0.5);
-        
-//        color = dark;
-        
-//        if (f_texpos.x < f_bounds.x) average = min(average, mix(average, 0.0, f_texpos.x/f_bounds.x));
-//        if (f_texpos.x > f_bounds.z) average = min(average, mix(average, 0.0, (1.0 - f_texpos.x)/(1.0 - f_bounds.z)));
-//        if (f_texpos.y < f_bounds.y) average = min(average, mix(average, 0.0, f_texpos.y/f_bounds.y));
-//        if (f_texpos.y > f_bounds.w) average = min(average, mix(average, 0.0, (1.0 - f_texpos.y)/(1.0 - f_bounds.w)));
-        
-//        avg + (vec4 - avg) * a
-        
-//        if (f_texpos.y < 0.5) color.xyz = mix(color.xyz, (vec3)(1.0),0.5);
         
         color.xyz = vec3(average);
         
         color = mix(color, dark, 1.0 - color.w);
-        
-//        color.w = 0.5;
     }
     // Else within bounds
-//    else if ((f_texpos.x < f_bounds.x + f_pixel_size) || (f_texpos.x > f_bounds.z - f_pixel_size) || (f_texpos.y < f_bounds.y + f_pixel_size) || (f_texpos.y > f_bounds.w - f_pixel_size))
-//    {
-//        color.xyz = 1.0 - color.xyz;
-//    }
     if ((f_texpos.x > f_bounds.x - f_pixel_size) && (f_texpos.x < f_bounds.x))
     {
         color = 1.0 - color;
